8a1c08ac714d059890581322382caacbe672c9e0
avlonitiss <avlonitiss@yahoo.co.uk>
2021-04-26 20:28:51 +0300

    Initialise Interface Shape

---
+++ src/main/java/iek/agdimitr/interface_lesson/Shape.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iek.agdimitr.interface_lesson;
+
+/**
+ *
+ * @author User
+ */
+public interface Shape {
+
+//δεν υπάρχει constructor    
+
+//abstract μέθοδοι
+    void draw();
+    double area();
+}
